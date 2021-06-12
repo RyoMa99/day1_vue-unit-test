ec3fca275837d79e193b521c511bd41bb828d553
RyoMa99 <hatanakaryoma.130@gmail.com>
2021-06-12 10:09:37 +0000

    lint

--- tests/unit/form.spec.ts
+++ tests/unit/form.spec.ts
@@ -8,10 +8,10 @@ describe('Form.vue', () => {
     input.setValue('John Doe')
     wrapper.trigger('submit')
     const emittedForm = wrapper.emitted('submitted')
-    if (emittedForm !== undefined){
+    if (emittedForm !== undefined) {
       console.log(emittedForm)
       expect(emittedForm[0][0]).toEqual({
-        name: "John Doe"
+        name: 'John Doe'
       })
     }
   })
